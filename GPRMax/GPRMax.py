2104c50c6d1474aca1a0ee565b66d872192810ed
Venkat Rajagopalan <Venkat Rajagopalan>
2015-11-22 17:58:17 -0500

    added

---
+++ GPRMax/GPRMax.py
@@ -0,0 +1,155 @@
+"""Control Agents based on TD Learning, i.e., Q-Learning and SARSA"""
+from rlpy.Agents.Agent import Agent, DescentAlgorithm
+from rlpy.Tools import addNewElementForAllActions, count_nonzero
+from random import sample
+import pyGPs
+import numpy as np
+import collections
+
+__copyright__ = ""
+__credits__ = [""]
+__license__ = "LGPL"
+
+
+class GPRMax(DescentAlgorithm, Agent):
+
+    #
+    # A crude implementation of the paper "Gaussian process for sample efficient 
+    # reinforcement learning with RMAX-like exploration" by
+    # Tobias Jung and Peter Stone
+    
+    # the current iteration
+    tick = 0
+
+    # the number of iterations to skip before training the GP's
+    trainEveryNSteps = 500;
+
+    # the local copy of state, difference between next state and current state, action and rewards
+    statesCache = collections.deque(maxlen=1e5)
+    diffStatesCache = collections.deque(maxlen=1e5)
+    actionsCache = collections.deque(maxlen=1e5)
+    rewardsCache = collections.deque(maxlen=1e5)
+
+    #
+    # the number of GP's depend on the dimensionality of the state and action space
+    #
+
+    # the dimensionality of the actions
+    actionsDim = 1;
+
+    # the dimensionality of the state
+    statesDim = 1;
+
+    # the list of Gaussian process objects - a total of actionsDim * statesDim 
+    transitionLearners = [];
+
+    # the reward model learner
+    rewardLearners = [];
+
+    # the histogram of actions seen so far
+    actionsHist = np.zeros(1);
+
+    def __init__(self, policy, representation, actionsDim, statesDim, discount_factor, lambda_=0, **kwargs):
+        super(
+            GPRMax,
+            self).__init__(policy=policy,
+            representation=representation, discount_factor=discount_factor, **kwargs)
+
+        # update the dimensionality of the actions space
+        self.actionsDim = actionsDim
+
+        # update the dimensionality of the state space
+        self.statesDim = statesDim
+
+        # instantiate the histogram of actions seen so far
+        self.actionHist = np.zeros(self.actionsDim)
+
+        # instantiate transitionLearners
+        self.transitionLearners = [pyGPs.GPR_FITC() for i in range(self.statesDim * self.actionsDim)]
+
+        # instantiate rewardLearners
+        self.rewardLearners = [pyGPs.GPR_FITC() for i in range(self.statesDim * self.actionsDim)]
+
+    def learn(self, s, p_actions, a, r, ns, np_actions, na, terminal):
+
+        # set prevStateTerminal to false and do the necessary calls to pre_discover
+        prevStateTerminal = False
+        self.representation.pre_discover(s, prevStateTerminal, a, ns, terminal)
+
+        # tick up the currentIteration
+        self.tick += 1
+      
+        # print for debug
+        if 0 == self.tick%100:
+            self.logger.info("Current iteration %d" % self.tick);
+
+        # update the actionsHist
+        self.actionHist[a] += 1
+        
+        # add s, a, r to the statesCache, actionsCache and rewardsCache
+        self.statesCache.append(s)
+        self.actionsCache.append(a)
+        self.diffStatesCache.append(ns-s)
+        self.rewardsCache.append(r)
+
+        # have the transistion and reward models been updated this iteration
+        modelUpdated = False
+        
+        # if it is time to learn - currently not limiting the number of samples
+        if self.tick % self.trainEveryNSteps == 0:
+            
+            # set the modelUpdated flag
+            modelUpdated = True
+
+            # dump the action distribution of samples seen so far
+            self.logger.info("Training on %d samples.\nSample Action Histogram" % self.tick)
+            self.logger.info(self.actionHist)
+            
+            # for every action, gather all the relevant states, diffStates and train
+            for actIdx in range(self.actionsDim):
+
+                # the start index for the transitionLearners
+                gpStartIdx = actIdx * self.statesDim
+
+                # find all the indexes in the actionsCache that match actIdx
+                indexes = [i for i,x in enumerate(self.actionsCache) if x == actIdx]
+
+                # allocate memory for the input, output
+                x = np.zeros(len(indexes))
+                y = np.zeros(len(indexes))
+                rwd = np.zeros(len(indexes))
+
+                # setup the input and output
+                for dim in range(self.statesDim):
+
+                    # iterate through, populate the inputs and outputs
+                    for idx,ii in zip(indexes, range(len(x))):
+                        x[ii] = self.statesCache[idx][dim]
+                        y[ii] = self.diffStatesCache[idx][dim]
+                        rwd[ii] = self.rewardsCache[idx]
+
+                    # pass the data along and learn 
+                    self.transitionLearners[gpStartIdx+dim].setData(x,y, value_per_axis=10)
+                    self.transitionLearners[gpStartIdx+dim].optimize()
+
+                    # pass the data along and learn
+                    self.rewardLearners[gpStartIdx+dim].setData(x,rwd, value_per_axis=10)
+                    self.rewardLearners[gpStartIdx+dim].optimize()
+ 
+        # If learning happened in this iteration, update the model that will be used
+        if modelUpdated:
+
+            phi_s = self.representation.phi(s, prevStateTerminal)
+            phi_sa = self.representation.phi_sa(s, prevStateTerminal, a, phi_s)            
+            a = self.policy.pi(s, terminal, p_actions)
+        
+        # expanded = self.representation.post_discover(
+        #     s,
+        #     prevStateTerminal,
+        #     a,
+        #     td_error,
+        #     phi_s)
+
+        if terminal:
+            # If THIS state is terminal:
+            self.episodeTerminated()
--- GPRMax/experiment.py
+++ GPRMax/experiment.py
@@ -1,1 +1,41 @@
-#!/usr/bin/env python
+from rlpy.Domains.HIVTreatment import HIVTreatment
+from rlpy.Policies import eGreedy
+from rlpy.Experiments import Experiment
+from rlpy.Representations import IncrementalTabular
+
+import numpy as np
+from GPRMax import GPRMax
+
+def make_experiment(
+        exp_id=1, path="./Results/Temp/{domain}/{agent}/{representation}/",
+        boyan_N0=238,
+        lambda_=0.9,
+        initial_learn_rate=.08,
+        discretization=35):
+    opt = {}
+    opt["exp_id"] = exp_id
+    opt["path"] = path
+    opt["max_steps"] = 5000
+    opt["num_policy_checks"] = 1
+    opt["checks_per_policy"] = 1
+
+    domain = HIVTreatment()
+    opt["domain"] = domain
+    representation = IncrementalTabular(domain)
+    policy = eGreedy(representation)
+    opt["agent"] = GPRMax(
+        policy, representation, domain.actions_num, len(domain.state_names),
+        discount_factor=domain.discount_factor,
+        lambda_=0.9,initial_learn_rate=initial_learn_rate,
+        learn_rate_decay_mode="boyan", boyan_N0=boyan_N0)
+    experiment = Experiment(**opt)
+    return experiment
+
+if __name__ == '__main__':
+    from rlpy.Tools.run import run_profiled
+    # run_profiled(make_experiment)
+    experiment = make_experiment(1)
+    experiment.run(visualize_learning=True)
+   # experiment.plot()
+    # experiment.save()
+
